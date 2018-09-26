718c6e6762c60156ac075c1a720d8931e871e7e3
bowans <bowans@gmail.com>
2018-09-26 11:57:27 +0800

    Implement zen of simplepage
    - python manage.py startapp zen
    - Implement index.html

--- simplepage/simplepage/urls.py
+++ simplepage/simplepage/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import zen.views
 
 urlpatterns = [
+    path('', zen.views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
